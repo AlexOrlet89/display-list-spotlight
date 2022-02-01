a77ca5c936d2ee4748eb34ddcc1f3252877b5386
Alex Orlet <AlexOrlet89@gmail.com>
2022-02-01 14:45:36 -0800

    fourtet.js is up now!

---
+++ fourtet.js
@@ -0,0 +1,17 @@
+export const fortetAlbums = [
+    {
+        name: 'Morning / Evening',
+        year: 2015,
+        tracks: ['Morning Side', 'Evening Side']
+    },
+    {
+        name: 'Parallel',
+        year: 2020,
+        tracks: ['Parallel 1', 'Parallel 2', 'Parallel 3', 'Parallel 4', 'Parallel 5', 'Parallel 6', 'Parallel 7', 'Parallel 8', 'Parallel 9', 'Parallel 10']
+    },
+    {
+        name: 'New Energy',
+        year: 2017,
+        tracks: ['Alap', 'Two Thousand and Seventeen', 'LA Trance', 'Tremper', 'Lush', 'Scientists', 'Falls 2', 'You Are Loved', 'SW9 9SL', '10 Midi', 'Memories', 'Daughter', 'Gentle Soul', 'Planet']
+    }
+];
